2b1af3446f116cdce484978238794dd7949dac2d
pushpendra <pushpendra090804@gmail.com>
2025-08-19 19:22:45 +0530

    changing server according to vercel

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,13 @@ import connectDB from "./src/db/db.js";
 
 connectDB();
 
+app.get("/", (req, res) => {
+  res.send({
+    activeStatus: true,
+    error: false,
+  })
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
